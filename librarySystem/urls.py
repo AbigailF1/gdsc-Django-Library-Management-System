74736c3097e125c476e4452b8eed4d933e947463
gebby123 <tefayegebrehiwot123@gmail.com>
2024-03-10 22:13:52 +0300

    try the nave bar

--- librarySystem/urls.py
+++ librarySystem/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='librarySystem'),
+    path('index/', views.index, name='index'),
+    path('staff/', views.staff, name='staff'),
 ]
--- librarySystem/views.py
+++ librarySystem/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('''
-
-''')
+    return render(request, 'dashbord/index.html')
+def staff(request):
+    return render(request, 'dashbord/staff.html')
